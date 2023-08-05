75b37a61086b8c96194b8bc9c126e40fa9829b1e
tblank <tblank@ieee.org>
2023-08-04 21:59:36 -0700

    fix setup

--- rvglue/setup.py
+++ rvglue/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rvglue',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'paho-mqtt==1.6.1'

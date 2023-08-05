3e4b7f66081930515cf17e19d4ab369359d8c351
John Oram <john.c.oram@gmail.com>
2023-08-04 22:45:40 -0700

    undio

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     package_dir={
         "": "rvglue",
     },
-    packages=find_packages(where="rvglue"),
+    packages=find_packages(),
     install_requires=[
         'paho-mqtt==1.6.1'
         ],

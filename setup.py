a7c2ba3d1c9d3a70c2250420fd3ae0bcf64bcabe
John Oram <john.c.oram@gmail.com>
2023-08-04 22:47:20 -0700

    hardcode

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     package_dir={
         "": "rvglue",
     },
-    packages=find_packages("rvglue"),
+    packages=["rvglue"],
     install_requires=[
         'paho-mqtt==1.6.1'
         ],

f6a9b5db6c99b33181fd6ab7c6c540f8878c4cc1
John Oram <john.c.oram@gmail.com>
2023-08-04 22:45:01 -0700

    undo

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     package_dir={
         "": "rvglue",
     },
-    py_modules=["rvglue"],
-    packages=find_packages(),
+    packages=find_packages(where="rvglue"),
     install_requires=[
         'paho-mqtt==1.6.1'
         ],
